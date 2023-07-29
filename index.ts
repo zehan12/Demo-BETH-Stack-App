3836dce0fcec88dcf7ac0e8bcba16195424cf9ad
Zehan Khan <zkhan@hootboard.com>
2023-07-30 02:57:40 +0530

    add html plugin and render html

--- index.ts
+++ index.ts
@@ -1,8 +1,22 @@
-import { Elysia } from 'elysia'
+import { Elysia } from "elysia";
+import { html } from "@elysiajs/html";
 
 const app = new Elysia()
-	.get('/', () => 'Hello Elysia')
-	.listen(8080)
-	 
-console.log(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`)
+  .use(html())
+  .get("/", ({ html }) => html(HTML))
+  .listen(8080);
+
+console.log(
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
+);
 console.log("Hello via Bun!");
+
+const HTML = `<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <title>🦊</title>
+    </head>
+    <body>
+        <h1>Hello Elysia </h1>
+    </body>
+</html>`;
