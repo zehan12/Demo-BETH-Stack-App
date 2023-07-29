ace552055ad646096889cb457208a442793f778c
Zehan Khan <zkhan@hootboard.com>
2023-07-30 02:49:16 +0530

    add elysia as web framework and create a basic server

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
+import { Elysia } from 'elysia'
+
+const app = new Elysia()
+	.get('/', () => 'Hello Elysia')
+	.listen(8080)
+	 
+console.log(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`)
 console.log("Hello via Bun!");
